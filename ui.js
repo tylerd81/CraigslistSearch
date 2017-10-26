1b74b0e2aebe6aa37d56a1faca06562d57f1043a
tylerd <drelyt@gmail.com>
2017-10-26 15:52:08 -0400

    More ui

--- ui.js
+++ ui.js
@@ -2,8 +2,8 @@ const blessed = require("blessed");
 const search = require("./search");
 
 const createResultsBox = function createResultsBox() {
-	let box = blessed.box({
-		height: '75%',
+	let box = blessed.list({
+		height: '50%',
 		top: 0,
 		left: 0,
 		border: {
@@ -13,13 +13,42 @@ const createResultsBox = function createResultsBox() {
 			bg: 'blue', 
 			border: {
 			fg:'white'
+			},
+			item : {
+				fg: 'white', 
+				bg: 'blue',
+			},
+			selected : {
+				fg: 'blue',
+				bg: 'white',
+				border : { fg: 'red'}
 			}
+		},
+		keys : {
+			down: 'd',
+			up : 'u'
 		}
 	});
 
 	
 	box.setLabel({text: "Results", side: "left"});
 	return box;
+}
+
+const createDetailsBox = function() {
+	let detailsBox = blessed.box( {
+		height: '25%',
+		top: '50%',
+		border: {
+			type: 'line'
+		},
+		style: {
+			fg: 'white',
+			border: {fg: 'white'}
+		}
+	});
+
+	return detailsBox;
 }
 
 const createSearchBox = function createSearchBox(searchTextReady) {
@@ -78,16 +107,23 @@ const defaultStatusBar = function(statusBar) {
 	statusBar.setText(menu);
 }
 
+const addResultsToList = function(results, list) {
+	results.map( (result) => list.add(result.itemName));
+}
+
 const start = function() {
 	
 	let screen = blessed.screen();
 	
 	let resultsBox = createResultsBox();
 	let statusBar = createStatusBar();
+	let detailsBox = createDetailsBox();
+	let currentResults = [];
 
 	defaultStatusBar(statusBar);
 
-	screen.key(['q','s'], (key)=> {
+	screen.key(['q','s','down','up', 'i'], (key)=> {
+
 		if(key === 'q') {
 			//only quit if the screen has the focus?
 			if(screen.focused) {
@@ -96,28 +132,43 @@ const start = function() {
 		}else if(key ==='s') {
 			searchBox.focus();
 			statusBar.setText("Search Mode");
-			screen.render();
+		}else if(key === 'u') {
+			resultsBox.up(1);
+		}else if(key === 'd') {
+			resultsBox.down(1);
 		}
+		screen.render();
 	});
 
 	let searchBox = createSearchBox((searchTerm) => {
 		//called when input is ready
 		
-		//clear the results box
-		resultsBox.setText("");
+		//clear the results 
+		resultsBox.clearItems();
 
 		search.searchFor(searchTerm, (results) => {
-			results.map( (result) => {
-				resultsBox.setText(resultsBox.getText() + result.itemName + "\n");
-			});
+
+			currentResults = results;
+			addResultsToList(results, resultsBox);	
+			resultsBox.focus();	
 			defaultStatusBar(statusBar);
 			screen.render();
 		});
 	});
 
+	resultsBox.on('select', (selected) => { 
+		//the index starts counting at 2(?)
+		let realIndex = selected.index - 2;
+		let selectedItem = currentResults[realIndex];
+
+		detailsBox.setText(`${selectedItem.itemName}`);
+		screen.render();
+	});
+
 	screen.append(resultsBox);
 	screen.append(searchBox);
 	screen.append(statusBar);
+	screen.append(detailsBox);
 	screen.render();
 }
 
